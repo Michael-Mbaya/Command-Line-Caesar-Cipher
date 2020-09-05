f7b9410557c3a11756e069e58c78e1de9894184a
Michael-Mbaya <gikundamike@gmail.com>
2020-09-06 01:04:34 +0300

    runProgram while loop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,24 @@
-//import java.io.BufferedReader;
 import java.io.Console;
-//import java.io.InputStreamReader;
-//import java.io.StringReader;
-//import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-
+        Boolean programRun = true;
+    while (programRun) {
         DataProperties dataProperties = new DataProperties();
 
-        Console optionConsole = System.console() ;
-
-        System.out.println("Choose an option '1' or '2'");
-        System.out.println("1. To Encrypt");
-        System.out.println("2. To Decrypt\n");
+        Console optionConsole = System.console();
+        System.out.println("Choose an option 'e' or 'd' or 'q': ");
+        System.out.println("e/E. To Encrypt");
+        System.out.println("d/D. To Decrypt");
+        System.out.println("q/Q. Exit\n");
 
         String option = optionConsole.readLine();
 
-        if ("1".equals(option)) {
+        if (option.equals("e") || option.equals("E")) {
             System.out.println("Enter word or letter to Encrypt\"\n");
             Console optionOneConsole = System.console();
-            String OptionOne = (optionOneConsole.readLine()).toLowerCase().replaceAll("[^\\p{L}\\p{Z}]","").replaceAll(" ","");
-            System.out.println(OptionOne);
+            String OptionOne = (optionOneConsole.readLine()).toLowerCase().replaceAll("[^\\p{L}\\p{Z}]", "").replaceAll(" ", "");
+//            System.out.println(OptionOne);
 
             System.out.println("Enter key number between (1-26) to encrypt\n");
             Console optionTwoConsole = System.console();
@@ -33,20 +30,22 @@ public class App {
             String enc = Encode.getDataProperties(OptionOne, chosenInt);
 //            String dec = Decode.getDataProperties(OptionOne, chosenInt);
 
-            System.out.println("Original Text: " + dataProperties.getChosenText().toUpperCase() );
-            System.out.println("Encrypted: " + enc.toUpperCase() + "\n" );
+            System.out.println("Original Text: " + dataProperties.getChosenText().toUpperCase());
+            System.out.println("Encrypted: " + enc.toUpperCase() + "\n");
             System.out.println("( *Encrypted Text has spaces and Punctuations removed from Original text )");
-//            System.out.println("Decrypted: " + dec);
+            //           System.out.println("Decrypted: " + dec);
 
-        } else if ("2".equals(option)) {
+        }
+        else if (option.equals("d") || option.equals("D")) {
             System.out.println("Enter word or letter to Decrypt");
             Console optionTwoConsole = System.console();
-            String optionTwo = (optionTwoConsole.readLine()).toLowerCase();
-//            String optionTwo = scanner.nextLine();
+            String optionTwo = (optionTwoConsole.readLine()).toLowerCase().replaceAll("[^\\p{L}\\p{Z}]", "").replaceAll(" ", "");
+            //            System.out.println(optionTwo.toUpperCase());
+            //            String optionTwo = scanner.nextLine();
 
             System.out.println("Enter the Shift key number (1-26) used to Encrypt the Word/Letter \n");
             Console shiftKeyConsole = System.console();
-            String chosenKeyNumber =shiftKeyConsole.readLine();
+            String chosenKeyNumber = shiftKeyConsole.readLine();
             int choseInt = Integer.parseInt(chosenKeyNumber);
 
             dataProperties.setChosenMyText(optionTwo);
@@ -54,9 +53,13 @@ public class App {
             String dec = Decode.getDataProperties(optionTwo, choseInt);
             System.out.println("Decrypted: " + dec);
 
-        } else {
-            System.out.println("Only words or letters allowed. Wrong Choice");
         }
+        else if (option.equals("q") || option.equals("Q")) {
+            programRun = false;
+        } else {
+            System.out.println("Wrong Option, Try Again!\n");
+        }
+    }
 
     }
 }
